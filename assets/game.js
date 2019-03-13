fc74f343dd1cfda6fbd57a5c0b68c506a9b17584
Fred Astaire <lancebd@thegodardgroup.com>
2019-03-13 16:07:17 -0400

    refined Restaurant Search

--- assets/game.js
+++ assets/game.js
@@ -3,30 +3,30 @@ var establishmentType = "";
 var zipLocation = "";
 var searchRadius = "";
 var costRange = "";
-var jokeQ="";
-var punchLine="";
+var jokeQ = "";
+var punchLine = "";
 
 function displayJokes() {
-  var queryURL = "https://official-joke-api.appspot.com/random_ten";
+    var queryURL = "https://official-joke-api.appspot.com/random_ten";
 
-  $.ajax({
-          url: queryURL,
-          method: "GET"
-      })
-      .then(function(response) {
-          console.log(response);
-          var results = response;
-          // cycle through each of the elements of the results array
-          for (i = 0; i < results.length; i++) {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response);
+            var results = response;
+            // cycle through each of the elements of the results array
+            for (i = 0; i < results.length; i++) {
 
-              jokeQ = $("#jokeQ").text(results[i].setup);
-              punchLine = $("#jokeGen").text(results[i].punchline);
-              
-          }
-      });
+                jokeQ = $("#jokeQ").text(results[i].setup);
+                punchLine = $("#jokeGen").text(results[i].punchline);
+
+            }
+        });
 }
 $(".card-link").on("click", function () {
- displayJokes();
+    displayJokes();
 })
 
 displayJokes();
@@ -38,17 +38,11 @@ $("#launch-search").on("click", function (event) {
     var establishmentType = $("#establishment-input").val().trim();
     console.log(establishmentType);
     var zipLocation = $("#location-input").val().trim();
+    checkLength();
     console.log(zipLocation);
     var searchMiles = $("#radius-input").val().trim();
     console.log(searchMiles);
     var costRange = $("#cost-input").val().trim();
-    if (costRange === "$") {
-        costRange = 1
-    } else if (costRange === "$$") {
-        costRange = 2
-    } else {
-        costRange = 3
-    }
     console.log(costRange);
     var searchRadius = searchMiles * 1600;
     console.log(searchRadius);
@@ -67,47 +61,49 @@ $("#launch-search").on("click", function (event) {
         .then(function (response) {
             console.log(response);
             for (var j = 0; j < 10; j++) {
-              var restaurantName = response.businesses[j].name;
-              console.log(restaurantName);
-              var restaurantAddress = response.businesses[j].location.address1 + ", " + response.businesses[j].location.city + ", " + response.businesses[j].location.state + " " + response.businesses[j].location.zip_code;
-              console.log(restaurantAddress);
-              var restaurantPhone = response.businesses[j].display_phone;
-              console.log(restaurantPhone);
-              var restaurantRating = response.businesses[j].rating;
-              console.log("Rating: " + restaurantRating);
-              createSearchCard(restaurantName,restaurantAddress, restaurantPhone, restaurantRating);
+                var restaurantName = response.businesses[j].name;
+                console.log(restaurantName);
+                var restaurantAddress = response.businesses[j].location.address1 + ", " + response.businesses[j].location.city + ", " + response.businesses[j].location.state + " " + response.businesses[j].location.zip_code;
+                console.log(restaurantAddress);
+                var restaurantPhone = response.businesses[j].display_phone;
+                console.log(restaurantPhone);
+                var restaurantRating = response.businesses[j].rating;
+                console.log("Rating: " + restaurantRating);
+                createSearchCard(restaurantName, restaurantAddress, restaurantPhone, restaurantRating);
             };
 
         })
-    
+
 });
 
 
-function createSearchCard (restaurantName, restaurantAddress, restaurantPhone, restaurantRating){
- 
-  var cardDiv = $('<div class="card border-light mb-3" "width:18rem;">');
-  var restName = $("<div class='card title'>").text(restaurantName);
-  var restAddress = $("<h5 class='card-text'>").text(restaurantAddress);
-  var restPhone = $("<div class ='card-text'>").text(restaurantPhone);
-  var restRating = $("<div class ='card-text'>").text(restaurantRating);
-  
-  cardDiv.append(restName);
-  cardDiv.append(restAddress);
-  cardDiv.append(restPhone);
-  cardDiv.append(restRating);
-  $("#resultsSpace").append(cardDiv);
-  
+function createSearchCard(restaurantName, restaurantAddress, restaurantPhone, restaurantRating) {
+
+    var cardDiv = $('<div class="card border-light mb-3" "width:18rem;">');
+    var restName = $("<div class='card title'>").text(restaurantName);
+    var restAddress = $("<h5 class='card-text'>").text(restaurantAddress);
+    var restPhone = $("<div class ='card-text'>").text(restaurantPhone);
+    var restRating = $("<div class ='card-text'>").text(restaurantRating);
+
+    cardDiv.append(restName);
+    cardDiv.append(restAddress);
+    cardDiv.append(restPhone);
+    cardDiv.append(restRating);
+    $("#resultsSpace").append(cardDiv);
+
 }
 
-// function renderHTML(url, headline, author, content) {
-//   $("#articles-space").empty();
-//   var articlesDiv = $("<div class='card article'>");
-//   var title = $("<p class='card title'>").html("<a target='_blank' href='" + url + "'>" + headline + "</a>");
-//   var byline = $("<p class='card-text'>").html(author);
-//   var blurb = $("<p class='card-text'>").html(content);
-//   articlesDiv.append(title);
-//   articlesDiv.append(byline);
-//   articlesDiv.append(blurb);
-//   articlesDiv.appendTo(myText);
-//   $("#articles-space").prepend(myText);
-// }
+function invalidZIPModal() {
+    var modal = document.getElementById('invalidZIPCode');
+    modal.style.display = "block";
+    modal.onclick = function () {
+        modal.style.display = "none";
+    }
+}
+
+function checkLength(zipLocation) {
+    var zip = $("#location-input").val().trim()
+    if (zip.length != 5) {
+        invalidZIPModal();
+    }
+}
